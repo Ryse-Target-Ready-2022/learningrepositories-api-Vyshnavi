57a1a59500230df4cd32d751df82d2b9ec8381bf
Vyshu293 <pasupuletisaivyshnavi@gmail.com>
2022-07-05 17:15:20 +0530

    initial commit

---
+++ src/main/java/com/tgt/rysetii/LearningresourcesApiSaiVyshnaviApplication.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningresourcesApiSaiVyshnaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningresourcesApiSaiVyshnaviApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgt/rysetii/LearningresourcesApiSaiVyshnaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgt.rysetii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningresourcesApiSaiVyshnaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
